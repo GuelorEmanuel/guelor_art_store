2147e55bb559d752ad99cd258a48660dfddb966c
gueloremanuel <ncstech07@gmail.com>
2020-03-06 18:26:54 -0500

    Fix unit test case

--- apps/art_store_web/test/art_store_web/controllers/page_controller_test.exs
+++ apps/art_store_web/test/art_store_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule ArtStoreWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Guelor Emanuel"
   end
 end
