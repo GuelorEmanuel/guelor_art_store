341c0f55af7cfdb29d2a1ec8aed7623fb7007f0a
gueloremanuel <ncstech07@gmail.com>
2020-03-06 13:50:38 -0500

    Remove import

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -25,7 +25,7 @@ secret_key_base =
 
 config :art_store_web, ArtStoreWeb.Endpoint,
   http: [
-    port: String.to_integer(System.get_env("PORT") || "443"),
+    port: String.to_integer(System.get_env("PORT") || "4000"),
     transport_options: [socket_opts: [:inet6]]
   ],
   secret_key_base: secret_key_base
