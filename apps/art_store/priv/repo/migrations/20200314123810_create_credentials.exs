dfe4762f2787f1f8ae3b54cc730a482e8a8014a4
gueloremanuel <ncstech07@gmail.com>
2020-03-14 08:41:47 -0400

    Create credential for user login Decided to decouple user and credential because of the possibility of being able to extend the system to support new credential features such as social login/auth login, etc.

---
+++ apps/art_store/priv/repo/migrations/20200314123810_create_credentials.exs
@@ -0,0 +1,16 @@
+defmodule ArtStore.Repo.Migrations.CreateCredentials do
+  use Ecto.Migration
+
+  def change do
+    create table(:credentials) do
+      add :email, :string, null: false
+      add :password_hash, :string
+      add :user_id, references(:users, on_delete: :nothing)
+
+      timestamps()
+    end
+
+    create unique_index(:credentials, [:email])
+    create index(:credentials, [:user_id])
+  end
+end
