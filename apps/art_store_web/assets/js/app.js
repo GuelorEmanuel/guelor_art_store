cfd83eec67c4697e99b354c357390531e724805d
gueloremanuel <ncstech07@gmail.com>
2020-04-06 05:09:05 -0400

    Fix querySelectorAll issue with remove input

--- apps/art_store_web/assets/js/app.js
+++ apps/art_store_web/assets/js/app.js
@@ -17,25 +17,30 @@ window.onload = () => {
   const removeElement = ({target}) => {
     let el = document.getElementById(target.dataset.id);
     let li = el.parentNode;
+
     li.parentNode.removeChild(li);
   }
+
   Array.from(document.querySelectorAll(".remove-form-field"))
   .forEach(el => {
      el.onclick = (e) => {
        removeElement(e);
      }
   });
+
   Array.from(document.querySelectorAll(".add-form-field"))
   .forEach(el => {
     el.onclick = ({target: {dataset}}) => {
       let container = document.getElementById(dataset.container);
       let index = container.dataset.index;
       let newRow = dataset.prototype;
-      container.insertAdjacentHTML("beforeend",       newRow.replace(/__name__/g, index));
+      container.insertAdjacentHTML("beforeend", newRow.replace(/__name__/g, index));
       container.dataset.index = parseInt(container.dataset.index) + 1;
-      container.querySelector("a.remove-form-field").onclick = (e) => {
-        removeElement(e);
-      }
+      container.querySelectorAll('a.remove-form-field').forEach(el => {
+        el.onclick = (e) => {
+          removeElement(e);
+        }
+      })
     }
   });
 }
